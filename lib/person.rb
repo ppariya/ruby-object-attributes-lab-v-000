2a58eb87464d2cb95fdb4b868c3cb8b8307dfa60
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-22 21:50:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
-  def name
-    @name = Fah
+  def name(name)
+    @name = name
   end
 
   def name= (new_name)
     @name = new_name
   end
 
-  def job
-    @job = QA
+  def job(job)
+    @job = job
   end
 
   def job= (new_job)
